750fa6c89269bd89462514eef1e75f66679db874
Noam Greenshtain <NoamG@neogames.com>
2019-07-21 07:06:14 +0300

    Fixed js procedures.

--- WebContent/js/weatherscript.js
+++ WebContent/js/weatherscript.js
@@ -1,8 +1,13 @@
 
 var table;
+var notFound;
 
 window.onload = function pre(){
 	try{
+		notFound = document.createElement("h1");
+		notFound.setAttribute("class","Title");
+		notFound.setAttribute("value","Oops! could not find location, please try again.");
+		
 		var col = ["City" , "Country", "Tempature", "Description","Illustration"]
         table = document.createElement("table");
         table.setAttribute("class", "myTable");
@@ -21,15 +26,19 @@ window.onload = function pre(){
 	}
 }
 
+
 function getText(){
 	try{
+		var p = document.getElementById("showData");
 		$(".search").animate({height : "140px"},600); //animates the div.
 		var p = document.getElementById("showData");
 		setTimeout(function(){ //shows up data after the slide.
+			try{
 			while (p.firstChild) { //removing components from previous searches.
 			    p.removeChild(p.firstChild);
 			}
-			table.deleteRow(1);
+			table.deleteRow(1);}
+			catch(e){console.log("Empty table.")}
 	    },599);
 
 	}
@@ -37,12 +46,9 @@ function getText(){
 		console.log(e);
 	}
 	try{
-		   /*setTimeout(function(){
-		    	
-		    },800);*/
 			var input = document.getElementById("myInput").value;
 			$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=" + input + "&APPID=93063b4a7b640155f9e086514f637714", function(data) { 
-				console.log(data);
+			console.log(data);
 			var im = "https://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
 			var temp = data.list[0].main.temp;
 			var v = document.createElement("img");
@@ -51,15 +57,13 @@ function getText(){
 			var desc = data.list[0].weather[0].description;
 			var city = data.city.name;
 			var country = data.city.country;
-			var p = document.getElementById("showData");
 			while (p.firstChild) { //removing components from previous searches.
 			    p.removeChild(p.firstChild);
 			}
-			
-            
+			       
             setTimeout(function(){ /*table initilization*/
     			tr = table.insertRow(-1);   			
-    			var values = [city,country,temp,desc,v];	
+    			var values = [city,country,Math.floor(temp-273.15) + " C",desc,v]; // temperature  is in Kelvin units.	
                 for (var j = 0; j < values.length; j++) {
                     var tabCell = document.createElement("td");
                     tr.appendChild(tabCell);
@@ -72,20 +76,21 @@ function getText(){
                 }
             	p.appendChild(table);
             },600);
-           
-			
+            
 		    $(".search").animate({height : "250px"},1000); //animates the div.
 		    setTimeout(function(){ //shows up data after the slide.
 		    	$(".myTable").append(table);
-		    }, 2200);
-	
-
-			
+		    }, 2200);		
 		}); 
 		}
 		catch(e){
-			alert(e);
-		}
-
+			
+			p.appendChild(notFound);
+			
+		    $(".search").animate({height : "250px"},1000); //animates the div.
+		    setTimeout(function(){ //shows up data after the slide.
+		    	$(".showData").append(notFound);
+		    }, 2200);
+			console.log(e);
+		}	
 }
-
